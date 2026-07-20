362d660b89188938c99f5b692190e9d71cd630e1
chatgpt-a01 <chatgpt.a01.2428478@gmail.com>
2026-07-20 00:32:39 +0000

    Add missing DocType controller modules

---
+++ sales_engagement_intelligence/sales_engagement_and_intelligence/doctype/sei_contact_role/sei_contact_role.py
@@ -0,0 +1,5 @@
+from frappe.model.document import Document
+
+
+class SEIContactRole(Document):
+    pass
---
+++ sales_engagement_intelligence/sales_engagement_and_intelligence/doctype/sei_playbook_contact_role/sei_playbook_contact_role.py
@@ -0,0 +1,5 @@
+from frappe.model.document import Document
+
+
+class SEIPlaybookContactRole(Document):
+    pass
